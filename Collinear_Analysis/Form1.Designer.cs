8cfe779978e925d351cec87f3df37283b0296411
Gleb <zadachings@gmail.com>
2018-12-01 17:01:22 +0200

    made collinear analysis

--- Collinear_Analysis/Form1.Designer.cs
+++ Collinear_Analysis/Form1.Designer.cs
@@ -28,13 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dt = new System.Windows.Forms.DataGridView();
+            this.GetFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CollinearBuild = new System.Windows.Forms.Button();
+            this.CollinearMatrix = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CollinearMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dt
+            // 
+            this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dt.Location = new System.Drawing.Point(12, 60);
+            this.dt.Name = "dt";
+            this.dt.RowTemplate.Height = 24;
+            this.dt.Size = new System.Drawing.Size(469, 562);
+            this.dt.TabIndex = 0;
+            // 
+            // GetFile
+            // 
+            this.GetFile.Location = new System.Drawing.Point(406, 11);
+            this.GetFile.Name = "GetFile";
+            this.GetFile.Size = new System.Drawing.Size(75, 23);
+            this.GetFile.TabIndex = 1;
+            this.GetFile.Text = "File";
+            this.GetFile.UseVisualStyleBackColor = true;
+            this.GetFile.Click += new System.EventHandler(this.GetFile_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(388, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // CollinearBuild
+            // 
+            this.CollinearBuild.Location = new System.Drawing.Point(487, 11);
+            this.CollinearBuild.Name = "CollinearBuild";
+            this.CollinearBuild.Size = new System.Drawing.Size(136, 45);
+            this.CollinearBuild.TabIndex = 3;
+            this.CollinearBuild.Text = "Build Collinear matrix";
+            this.CollinearBuild.UseVisualStyleBackColor = true;
+            this.CollinearBuild.Click += new System.EventHandler(this.CollinearBuild_Click);
+            // 
+            // CollinearMatrix
+            // 
+            this.CollinearMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CollinearMatrix.Location = new System.Drawing.Point(496, 95);
+            this.CollinearMatrix.Name = "CollinearMatrix";
+            this.CollinearMatrix.RowTemplate.Height = 24;
+            this.CollinearMatrix.Size = new System.Drawing.Size(301, 150);
+            this.CollinearMatrix.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(594, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Collinear Matrix";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1518, 626);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CollinearMatrix);
+            this.Controls.Add(this.CollinearBuild);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GetFile);
+            this.Controls.Add(this.dt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CollinearMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dt;
+        private System.Windows.Forms.Button GetFile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button CollinearBuild;
+        private System.Windows.Forms.DataGridView CollinearMatrix;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Collinear_Analysis/Form1.cs
+++ Collinear_Analysis/Form1.cs
@@ -1,20 +1,146 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Reflection;
+using ExcelObj = Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Collinear_Analysis
 {
     public partial class Form1 : Form
     {
+        List<double> first = new List<double>();
+        List<double> second = new List<double>();
+        List<double> third = new List<double>();
+        List<double> fourth = new List<double>();
+        List<double> fifth = new List<double>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void GetFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Файл Excel|*.XLSX;*.XLS"; ;
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+                OleDbConnection connection = new OleDbConnection(string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES\";", dialog.FileName));
+                OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [Лист1$]", connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dt.DataSource = table;
+                connection.Close();
+            }
+        }
+
+        private void GetData()
+        {
+            var data = dt.Rows.OfType<DataGridViewRow>().Where(x => x.Cells[0].Value != null).Select(
+                            r => r.Cells.OfType<DataGridViewCell>().Select(c => c.Value).ToArray()).ToList();
+
+
+            foreach (var item in data)
+            {
+                first.Add(double.Parse(item[0].ToString()));
+                second.Add(double.Parse(item[1].ToString()));
+                third.Add(double.Parse(item[2].ToString()));
+                fourth.Add(double.Parse(item[3].ToString()));
+                fifth.Add(double.Parse(item[4].ToString()));
+            }
+        }
+
+        private void CollinearBuild_Click(object sender, EventArgs e)
+        {
+            double Kv_Kv = 0;
+            double Kv_Ibp = 0;
+            double Kv_Zp = 0;
+            double Kv_Isc = 0;
+            double Kv_Vzr = 0;
+
+            double Ibp_Ibp = 0;
+            double Ibp_Zp = 0;
+            double Ibp_Isc = 0;
+            double Ibp_Vzr = 0;
+
+            double Zp_Zp = 0;
+            double Zp_Isc = 0;
+            double Zp_Vzr = 0;
+
+            double Isc_Isc = 0;
+            double Isc_Vzr = 0;
+            double Vzr_Vzr = 0;
+
+            if (dt.DataSource == null)
+                MessageBox.Show("Open excel file with data");
+
+            else
+            {
+                GetData();
+
+                Kv_Kv = Correlation(first, first);
+                Kv_Ibp = Correlation(first, second);
+                Kv_Isc = Correlation(first, third);
+                Kv_Zp = Correlation(first, fourth);
+                Kv_Vzr = Correlation(first, fifth);
+
+                Ibp_Ibp = Correlation(second, second);
+                Ibp_Isc = Correlation(second, third);
+                Ibp_Zp = Correlation(second, fourth);
+                Ibp_Vzr = Correlation(second, fifth);
+
+                Zp_Zp = Correlation(fourth, fourth);
+                Zp_Isc = Correlation(fourth, third);
+                Zp_Vzr = Correlation(fourth, fifth);
+
+                Isc_Isc = Correlation(third, third);
+                Isc_Vzr = Correlation(third, fifth);
+
+                Vzr_Vzr = Correlation(fifth, fifth);
+            }
+        }
+
+        private double Correlation(List<double> first, List<double> second)
+        {
+            double[] array_xy = new double[first.Count];
+            double[] array_xp2 = new double[first.Count];
+            double[] array_yp2 = new double[first.Count];
+            for (int i = 0; i < first.Count; i++)
+                array_xy[i] = first[i] * second[i];
+            for (int i = 0; i < first.Count; i++)
+                array_xp2[i] = Math.Pow(first[i], 2.0);
+            for (int i = 0; i < first.Count; i++)
+                array_yp2[i] = Math.Pow(second[i], 2.0);
+            double sum_x = 0;
+            double sum_y = 0;
+            foreach (double n in first)
+                sum_x += n;
+            foreach (double n in second)
+                sum_y += n;
+            double sum_xy = 0;
+            foreach (double n in array_xy)
+                sum_xy += n;
+            double sum_xpow2 = 0;
+            foreach (double n in array_xp2)
+                sum_xpow2 += n;
+            double sum_ypow2 = 0;
+            foreach (double n in array_yp2)
+                sum_ypow2 += n;
+            double Ex2 = Math.Pow(sum_x, 2.00);
+            double Ey2 = Math.Pow(sum_y, 2.00);
+
+            double Correl =
+            (first.Count * sum_xy - sum_x * sum_y) /
+            Math.Sqrt((first.Count * sum_xpow2 - Ex2) * (first.Count * sum_ypow2 - Ey2));
+
+            return Correl;
         }
     }
 }
