eec93e6134c7d4451e264c9fa3d6d160fbb1bbd6
Gleb <zadachings@gmail.com>
2018-12-03 17:18:06 +0200

    made datagrid view w/ normalized values of input matrix

--- Collinear_Analysis/Form1.Designer.cs
+++ Collinear_Analysis/Form1.Designer.cs
@@ -29,51 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.dt = new System.Windows.Forms.DataGridView();
-            this.GetFile = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.matr = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.analysisTb = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
+            this.dt = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Collinear = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.normDt = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.matr)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.Collinear.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.normDt)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dt
-            // 
-            this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dt.Location = new System.Drawing.Point(12, 60);
-            this.dt.Name = "dt";
-            this.dt.RowTemplate.Height = 24;
-            this.dt.Size = new System.Drawing.Size(469, 475);
-            this.dt.TabIndex = 0;
-            // 
-            // GetFile
-            // 
-            this.GetFile.Location = new System.Drawing.Point(406, 11);
-            this.GetFile.Name = "GetFile";
-            this.GetFile.Size = new System.Drawing.Size(75, 23);
-            this.GetFile.TabIndex = 1;
-            this.GetFile.Text = "File";
-            this.GetFile.UseVisualStyleBackColor = true;
-            this.GetFile.Click += new System.EventHandler(this.GetFile_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(388, 22);
-            this.textBox1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(859, 269);
+            this.label1.Location = new System.Drawing.Point(1095, 299);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(147, 25);
             this.label1.TabIndex = 5;
@@ -84,47 +68,37 @@
             this.matr.AllowUserToAddRows = false;
             this.matr.AllowUserToDeleteRows = false;
             this.matr.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.matr.Location = new System.Drawing.Point(505, 297);
+            this.matr.Location = new System.Drawing.Point(757, 327);
             this.matr.MultiSelect = false;
             this.matr.Name = "matr";
             this.matr.ReadOnly = true;
             this.matr.RowTemplate.Height = 24;
-            this.matr.Size = new System.Drawing.Size(850, 238);
+            this.matr.Size = new System.Drawing.Size(788, 238);
             this.matr.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(496, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(136, 45);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Build Collinear matrix";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(985, 95);
+            this.textBox2.Location = new System.Drawing.Point(1177, 65);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(386, 130);
+            this.textBox2.Size = new System.Drawing.Size(368, 130);
             this.textBox2.TabIndex = 7;
             this.textBox2.Text = resources.GetString("textBox2.Text");
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // analysisTb
             // 
-            this.analysisTb.Location = new System.Drawing.Point(505, 95);
+            this.analysisTb.Location = new System.Drawing.Point(757, 65);
             this.analysisTb.Multiline = true;
             this.analysisTb.Name = "analysisTb";
-            this.analysisTb.Size = new System.Drawing.Size(443, 130);
+            this.analysisTb.Size = new System.Drawing.Size(411, 130);
             this.analysisTb.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(1106, 67);
+            this.label2.Location = new System.Drawing.Point(1296, 36);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(157, 25);
             this.label2.TabIndex = 9;
@@ -134,48 +108,159 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(664, 67);
+            this.label3.Location = new System.Drawing.Point(929, 37);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 25);
             this.label3.TabIndex = 10;
             this.label3.Text = "Analysis";
             // 
+            // dt
+            // 
+            this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dt.Location = new System.Drawing.Point(19, 36);
+            this.dt.Name = "dt";
+            this.dt.RowTemplate.Height = 24;
+            this.dt.Size = new System.Drawing.Size(722, 548);
+            this.dt.TabIndex = 0;
+            this.dt.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dt_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Collinear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Collinear);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1610, 657);
+            this.tabControl1.TabIndex = 14;
+            // 
+            // Collinear
+            // 
+            this.Collinear.Controls.Add(this.dt);
+            this.Collinear.Controls.Add(this.button2);
+            this.Collinear.Controls.Add(this.label1);
+            this.Collinear.Controls.Add(this.button1);
+            this.Collinear.Controls.Add(this.matr);
+            this.Collinear.Controls.Add(this.textBox1);
+            this.Collinear.Controls.Add(this.textBox2);
+            this.Collinear.Controls.Add(this.label2);
+            this.Collinear.Controls.Add(this.analysisTb);
+            this.Collinear.Controls.Add(this.label3);
+            this.Collinear.Location = new System.Drawing.Point(4, 25);
+            this.Collinear.Name = "Collinear";
+            this.Collinear.Padding = new System.Windows.Forms.Padding(3);
+            this.Collinear.Size = new System.Drawing.Size(1602, 628);
+            this.Collinear.TabIndex = 0;
+            this.Collinear.Text = "Collinear Analysis";
+            this.Collinear.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.normDt);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1602, 628);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Multilollinear analysis";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(654, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // normDt
+            // 
+            this.normDt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.normDt.Location = new System.Drawing.Point(8, 35);
+            this.normDt.Name = "normDt";
+            this.normDt.RowTemplate.Height = 24;
+            this.normDt.Size = new System.Drawing.Size(590, 585);
+            this.normDt.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(200, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Normalized values";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1383, 552);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.analysisTb);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.matr);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.GetFile);
-            this.Controls.Add(this.dt);
+            this.ClientSize = new System.Drawing.Size(1610, 657);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.matr)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.Collinear.ResumeLayout(false);
+            this.Collinear.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.normDt)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dt;
-        private System.Windows.Forms.Button GetFile;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView matr;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox analysisTb;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Collinear;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView normDt;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Collinear_Analysis/Form1.cs
+++ Collinear_Analysis/Form1.cs
@@ -13,6 +13,9 @@ namespace Collinear_Analysis
 {
     public partial class Form1 : Form
     {
+        double[,] rez;
+        int sumRow = 0;
+        
         double min = 0;
         double max = 0;
         int mini = 0, minj = 0;
@@ -25,23 +28,7 @@ namespace Collinear_Analysis
 
         private void GetFile_Click(object sender, EventArgs e)
         {
-            dt.DataSource = string.Empty;
-            matr.Columns.Clear();
-
-
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "Файл Excel|*.XLSX;*.XLS"; ;
-
-            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                textBox1.Text = dialog.FileName;
-                OleDbConnection connection = new OleDbConnection(string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES\";", dialog.FileName));
-                OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [Лист1$]", connection);
-                DataTable table = new DataTable();
-                adapter.Fill(table);
-                dt.DataSource = table;
-                connection.Close();
-            }
+            
         }
 
         private void Setmatr()
@@ -106,13 +93,38 @@ namespace Collinear_Analysis
 
         }
 
-        private void Analysis()
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            analysisTb.Text = "The smallest correlation coefficient is: " + min + " of " + matr.Rows[mini].HeaderCell.Value + " to " + matr.Rows[minj].HeaderCell.Value + "\n" +
-                "The highest correlation coefficient is: " + max + " of " + matr.Rows[maxi].HeaderCell.Value + " to " + matr.Rows[maxj].HeaderCell.Value;
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void dt_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            dt.DataSource = string.Empty;
+            matr.Columns.Clear();
+
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Файл Excel|*.XLSX;*.XLS"; ;
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+                OleDbConnection connection = new OleDbConnection(string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0 Xml;HDR=YES\";", dialog.FileName));
+                OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM [Лист1$]", connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dt.DataSource = table;
+                connection.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
             int i = 0;
             int j = 0;
@@ -233,6 +245,179 @@ namespace Collinear_Analysis
             }
 
             Analysis();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            int j = 0;
+            double[,] matrs;
+            matrs = new double[dt.RowCount-1, dt.ColumnCount];
+
+            for (i = 0; i < matrs.GetLength(0); i++)
+            {
+                for (j = 0; j < matrs.GetLength(1); j++)
+                {
+                    matrs[i, j] = Convert.ToDouble(dt.Rows[i].Cells[j /*+ 1*/].Value);
+                }
+            }
+
+            double[] avr;
+            double[] sumkv;
+            double[] disp;
+            double[,] matrs2;
+            matrs2 = new double[dt.RowCount-1, dt.ColumnCount];
+            sumRow = matrs.GetLength(0);// - 1;
+            avr = new double[matrs.GetLength(1)];
+            sumkv = new double[matrs.GetLength(1)];
+            disp = new double[matrs.GetLength(1)];
+            double summ = 0;
+            // Середнє
+            for (j = 0; j < matrs.GetLength(1); j++)
+            {
+                summ = 0;
+                for (i = 0; i < matrs.GetLength(0); i++)
+                {
+                    summ += matrs[i, j];
+                }
+                avr[j] = summ / sumRow;
+            }
+            // Дисперсія
+            for (j = 0; j < matrs.GetLength(1); j++)
+            {
+                summ = 0;
+                for (i = 0; i < matrs.GetLength(0) /*- 1*/; i++)
+                {
+                    summ += (matrs[i, j] - avr[j]) * (matrs[i, j] - avr[j]);
+                }
+                disp[j] = summ / sumRow;
+            }
+            for (j = 0; j < matrs.GetLength(1); j++)
+                Console.WriteLine(" di ={0}", disp[j]);
+            // Матриця кореляції
+            //label9.Text = "Матриця стандартизованих незалежних змінних"; ;\\
+
+            normDt.Columns.Add(new DataGridViewColumn() { HeaderText = dt.Columns[0].HeaderText, CellTemplate = new DataGridViewTextBoxCell() });
+            normDt.Columns.Add(new DataGridViewColumn() { HeaderText = dt.Columns[1].HeaderText, CellTemplate = new DataGridViewTextBoxCell() });
+            normDt.Columns.Add(new DataGridViewColumn() { HeaderText = dt.Columns[2].HeaderText, CellTemplate = new DataGridViewTextBoxCell() });
+            normDt.Columns.Add(new DataGridViewColumn() { HeaderText = dt.Columns[3].HeaderText, CellTemplate = new DataGridViewTextBoxCell() });
+            normDt.Columns.Add(new DataGridViewColumn() { HeaderText = dt.Columns[4].HeaderText, CellTemplate = new DataGridViewTextBoxCell() });
+
+            int n = matrs.GetLength(0);// - 1;
+            for (i = 0; i < n/*matrs.GetLength(0) - 1*/; i++)
+            {
+                for (j = 0; j < matrs.GetLength(1); j++)
+                {
+                    matrs2[i, j] = (matrs[i, j] - avr[j]) / Math.Sqrt(disp[j] * sumRow);
+                    normDt.Rows.Add();
+                    normDt.Rows[i].Cells[j].Value = Math.Round(matrs2[i, j] * 1000,2) / 1000.0;
+                }
+
+            }
+            
+            //label8.Text = "Кореляційна матриця";
+            //int k = 0;
+            //int l = 0;
+            //double sumxy = 0;
+            //double avrxy = 0;
+            //double koefkorel = 0;
+            //dataGridView2.RowCount = matrs.GetLength(1);
+            //dataGridView2.ColumnCount = matrs.GetLength(1);
+            //for (l = 0; l < matrs.GetLength(1); l++)
+            //{
+            //    for (k = 0; k < matrs.GetLength(1); k++)
+            //    {
+            //        sumxy = 0;
+            //        for (i = 0; i < sumRow; i++)
+            //        {
+            //            sumxy = sumxy + matrs[i, l] * matrs[i, k];
+            //        }
+            //        avrxy = sumxy / sumRow;
+            //        koefkorel = (avrxy - avr[k] * avr[l]) / (Math.Sqrt(disp[l]) * Math.Sqrt(disp[k]));
+            //        dataGridView2.Rows[l].Cells[k].Value = koefkorel;
+            //    }
+            //    dataGridView2.Columns[l].HeaderText = dataGridView1.Columns[l + 1].HeaderText;
+            //    dataGridView2.Rows[l].HeaderCell.Value = dataGridView1.Columns[l + 1].HeaderText;
+            //}
+
+            rez = new double[matr.RowCount, matr.ColumnCount];
+            for (i = 0; i < matr.RowCount; i++)
+            {
+                for (j = 0; j < matr.ColumnCount; j++)
+                {
+                    rez[i, j] = Convert.ToDouble(matr.Rows[i].Cells[j].Value);
+                }
+            }
+            // Визначник матриці, ХІ - квадрат
+
+            const double xitable = 24.996;
+            double det = Determ(rez);
+            Console.WriteLine(det);
+            //textBox3.Text = Convert.ToString(det);
+            MessageBox.Show("determinant: "+det.ToString());
+            double xi = -(sumRow - 1 - 1 / 6 * (2 * matrs.GetLength(1) + 5)) * Math.Log(det, Math.E);
+            //textBox4.Text = Convert.ToString(xi);
+            MessageBox.Show("xi: " + xi.ToString());
+            if (xi > xitable)
+            {
+                textBox1.Text = "В масиві незалежних змінних існує мультиколінеарність.";
+                //button5.Enabled = true;
+                MessageBox.Show("something to turn on");
+            }
+            else
+            {
+                MessageBox.Show("Аналіз завершено. Мультиколінеарності неіснує!");
+            }
+            button3.Enabled = false;
+
+        }
+
+
+        public static double[,] GetMinor(double[,] matrix, int row, int column)
+        {
+            double[,] buf = new double[matrix.GetLength(0) - 1, matrix.GetLength(0) - 1];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if ((i != row) || (j != column))
+                    {
+                        if (i > row && j < column) buf[i - 1, j] = matrix[i, j];
+                        if (i < row && j > column) buf[i, j - 1] = matrix[i, j];
+                        if (i > row && j > column) buf[i - 1, j - 1] = matrix[i, j];
+                        if (i < row && j < column) buf[i, j] = matrix[i, j];
+                    }
+                }
+            return buf;
+        }
+
+        public static double Determ(double[,] matrix)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new Exception(" Число строк в матрице не совпадает с числом столбцов");
+            double det = 0;
+            int Rank = matrix.GetLength(0);
+            if (Rank == 1) det = matrix[0, 0];
+            if (Rank == 2) det = matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            if (Rank > 2)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    det += Math.Pow(-1, 0 + j) * matrix[0, j] * Determ(GetMinor(matrix, 0, j));
+                }
+            }
+            return det;
+        }
+
+
+
+        private void Analysis()
+        {
+            analysisTb.Text = "The smallest correlation coefficient is: " + min + " of " + matr.Rows[mini].HeaderCell.Value + " to " + matr.Rows[minj].HeaderCell.Value + "\n" +
+                "The highest correlation coefficient is: " + max + " of " + matr.Rows[maxi].HeaderCell.Value + " to " + matr.Rows[maxj].HeaderCell.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 
